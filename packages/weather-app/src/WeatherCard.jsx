11102f69231f48dcdc4c4f93c623bad99a422685
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-20 15:45:37 -0500

    Styling specific to component UI

--- packages/weather-app/src/WeatherCard.jsx
+++ packages/weather-app/src/WeatherCard.jsx
@@ -1,15 +1,22 @@
 import NextDays from './NextDays';
+import './WeatherCard.css';
+import sunny from '../public/sunny.svg';
 
 export default function WeatherCard() {
   return (
-    <div>
-      <div>
-        <h3>Today</h3>
-        <h1>New York</h1>
-        <h3>Temperature: 17° C</h3>
-        <h3>clear sky</h3>
+    <div id="weather-card">
+      <div id="today">
+        <div className="left">
+          <img id="main-svg" src={sunny} alt="sun" />
+        </div>
+        <div className="right">
+          <h3>Today</h3>
+          <h1>New York</h1>
+          <h3>Temperature: 17° C</h3>
+          <h3>clear sky</h3>
+        </div>
       </div>
-      <div>
+      <div id="forecast">
         <NextDays day="Wednesday" temp="21°C" weathercode="0" />
         <NextDays day="Thursday" temp="24°C" weathercode="3" />
         <NextDays day="Friday" temp="21°C" weathercode="0" />
