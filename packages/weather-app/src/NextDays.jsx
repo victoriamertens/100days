d369c98ff45724193e874b9fccbd7d6b08ea44fc
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-22 09:38:47 -0500

    Added remaining svg for forecast, and final touch styling

--- packages/weather-app/src/NextDays.jsx
+++ packages/weather-app/src/NextDays.jsx
@@ -1,11 +1,28 @@
 import sunny from '/src/sunny.svg';
+import cloudy from '/src/cloudy.svg';
+import partly from '/src/partly.svg';
 import './NextDays.css';
 
 export default function NextDays(props) {
   return (
     <div className="forecast">
       <p className="day">{props.day}</p>
-      <img className="forecast-icon" src={sunny} alt="sun" />
+      {props.weathercode === '3' ? (
+        <img className="forecast-icon" src={cloudy} alt="cloud" />
+      ) : (
+        <></>
+      )}
+      {props.weathercode === '0' ? (
+        <img className="forecast-icon" src={sunny} alt="sun" />
+      ) : (
+        <></>
+      )}
+      {props.weathercode === '2' ? (
+        <img className="forecast-icon" src={partly} alt="partly" />
+      ) : (
+        <></>
+      )}
+
       <p className="temp">{props.temp}</p>
     </div>
   );
