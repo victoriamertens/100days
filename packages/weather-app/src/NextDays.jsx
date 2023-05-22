4bc144e5b6c46d0e97076636f229657a0c7ac9ee
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-22 08:51:07 -0500

    Forecast Cards styled to match UI

--- packages/weather-app/src/NextDays.jsx
+++ packages/weather-app/src/NextDays.jsx
@@ -1,11 +1,12 @@
 import sunny from '/src/sunny.svg';
+import './NextDays.css';
 
 export default function NextDays(props) {
   return (
-    <>
-      <p>{props.day}</p>
-      <img src={sunny} alt="sun" />
-      <p>{props.temp}</p>
-    </>
+    <div className="forecast">
+      <p className="day">{props.day}</p>
+      <img className="forecast-icon" src={sunny} alt="sun" />
+      <p className="temp">{props.temp}</p>
+    </div>
   );
 }
