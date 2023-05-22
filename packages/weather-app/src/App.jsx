6ae1246faea8eb38919e72a8e57d016e45ec2b2b
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-22 08:14:36 -0500

    Input Styled to match UI

--- packages/weather-app/src/App.jsx
+++ packages/weather-app/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div id="whole-app">
-      <input placeholder="Enter a City..." type="text"></input>
+      <input placeholder="Enter a City..." type="text" id="city-input"></input>
       <WeatherCard />
     </div>
   );
