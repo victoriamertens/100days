c8fa44306f3b8a9f28421dd6b60e6d3802bf43df
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-23 10:41:06 -0500

    Functions firing properly, tested

--- packages/weather-app/src/App.jsx
+++ packages/weather-app/src/App.jsx
@@ -13,8 +13,11 @@ function App() {
           placeholder="Enter a City..."
           type="text"
           id="city-input"
+          onChange={(e) => setInput(e.target.value)}
         ></input>
-        <button id="search-btn">Search</button>
+        <button id="search-btn" onClick={() => console.log(input)}>
+          Search
+        </button>
       </div>
       <WeatherCard response={response} />
     </div>
