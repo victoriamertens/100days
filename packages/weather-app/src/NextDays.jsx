0b72e3201996b1399d7874cb68c33163f3fd2a72
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-23 10:19:02 -0500

    Future days component now dynamic

--- packages/weather-app/src/NextDays.jsx
+++ packages/weather-app/src/NextDays.jsx
@@ -1,11 +1,22 @@
 import './NextDays.css';
 
 export default function NextDays(props) {
+  let dayOfWeek = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  };
+
+  let temp = props.info.main.temp;
   return (
     <div className="forecast">
-      <p className="day">{props.day}</p>
+      <p className="day">{dayOfWeek[props.day]}</p>
       <img className="forecast-icon" src={props.weathericon} alt="cloud" />
-      <p className="temp">{props.temp}</p>
+      <p className="temp">{temp}</p>
     </div>
   );
 }
--- packages/weather-app/src/WeatherCard.jsx
+++ packages/weather-app/src/WeatherCard.jsx
@@ -6,8 +6,10 @@ import { codeToIcon } from '../weatherCodes';
 export default function WeatherCard(responseObj) {
   let dayForecastArr = responseObj.response.list;
 
-  let today = dayForecastArr[0];
-  let iconToday = codeToIcon(today.weather[0].id);
+  let todayData = dayForecastArr[0];
+  let iconToday = codeToIcon(todayData.weather[0].id);
+  let todayCode = new Date().getDay();
+  let todayCounter = 0;
 
   return (
     <div id="weather-card">
@@ -18,17 +20,19 @@ export default function WeatherCard(responseObj) {
         <div className="right">
           <h3 id="day">Today</h3>
           <h1 id="location">New York</h1>
-          <h3 id="temp">Temperature: {today.main.temp}° F</h3>
-          <h3 id="des">{today.weather[0].description}</h3>
+          <h3 id="temp">Temperature: {todayData.main.temp}° F</h3>
+          <h3 id="des">{todayData.weather[0].description}</h3>
         </div>
       </div>
       <div id="forecast">
         {dayForecastArr.map((day) => {
           if (day !== dayForecastArr[0]) {
+            todayCounter++;
             return (
               <NextDays
                 key={day.dt}
                 info={day}
+                day={todayCode + todayCounter}
                 weathericon={codeToIcon(dayForecastArr[1].weather[0].id)}
               />
             );
