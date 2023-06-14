07eff54d25e2861d4b70dafbc9e150b79ef082ce
Victoria Mertens <victoria.mertens44@gmail.com>
2023-06-14 11:30:17 -0500

    Reconfig to include Vite

--- packages/chat-app/client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import App from './App.jsx';
-import { createRoot } from 'react-dom/client';
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
