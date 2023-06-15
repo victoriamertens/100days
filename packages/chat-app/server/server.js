c64f2f826077abc57647226928a0f6ae10d06cce
Victoria Mertens <victoria.mertens44@gmail.com>
2023-06-15 08:46:35 -0500

    Updates to integrate vite server

--- packages/chat-app/server/server.js
+++ packages/chat-app/server/server.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+import express from 'express';
+import { createServer as createViteServer } from 'vite';
+import pg from 'pg';
 
-const app = express();
-
-// Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files
-app.use(express.static('build'));
-
-// App Set //
-const PORT = 5000;
-
-/** Listen * */
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+let pool = new pg.Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'auth_shelf',
 });
+
+async function createServer() {
+  const app = express();
+
+  // Create Vite server in middleware mode and configure the app type as
+  // 'custom', disabling Vite's own HTML serving logic so parent server
+  // can take control
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  });
+
+  // Use vite's connect instance as middleware. If you use your own
+  // express router (express.Router()), you should use router.use
+  app.use(vite.middlewares);
+
+  app.use('*', async (req, res) => {
+    // serve index.html - we will tackle this next
+    pool.query();
+  });
+
+  app.listen(5173);
+}
+
+createServer();
--- packages/chat-app/vite.config.ts
+++ packages/chat-app/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
   server: {
     proxy: {
       '/api': {
