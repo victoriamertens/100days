8366783a2268ab5f9b4d6d106c9bb69252c2151d
Victoria Mertens <victoria.mertens44@gmail.com>
2023-06-30 07:25:15 -0500

    Skeleton for API calls to API Gateway/Lambda

--- packages/weather-app/APIs.js
+++ packages/weather-app/APIs.js
@@ -1,5 +1,9 @@
 // import axios from 'axios';
-import utils from './utils';
+import {
+  coordinatesAPIUrl as hiddenAPIUrl,
+  weatherAPIUrl as hiddedWeatherAPIUrl,
+} from './HiddenAPI';
+import axios from 'axios';
 
 //Decided to implement Amazon's Lambda service to handle the http request to keep app fully frontend/serverless
 // axios({
@@ -14,20 +18,35 @@ import utils from './utils';
 //   .catch((err) => console.log(err));
 
 const cityToWeather = async (city) => {
-  let coordinates = await utils.fetchCoordinates(city);
-  let weather = await utils.fetchWeather(coordinates);
+  let coordinates = await fetchCoordinates(city);
+  let weather = await fetchWeather(coordinates);
   return weather;
 };
 
-const fetchCoordinates = (city) => {
-  console.log('In Coorinates', city);
-  return 0;
+const fetchCoordinates = async (city) => {
+  console.log('In fetchCoordinates', city);
+  let url = hiddenAPIUrl + city;
+  let response;
+  await axios({ method: 'GET', url: url })
+    .then((res) => (response = res))
+    .catch((err) => console.log('fetchCoordinates Errored:', err));
+  console.log('fetchCoordinates response:', response);
+  return response;
 };
 
-const fetchWeather = (coordinates) => {
-  console.log('In weather', coordinates);
-  return 0;
+const fetchWeather = async (coordinates) => {
+  console.log('In fetchWeather', coordinates);
+  let url =
+    hiddedWeatherAPIUrl + 'lat=' + coordinates.lat + '&lon=' + coordinates.lon;
+
+  let response;
+  await axios({ method: 'GET', url: url })
+    .then((res) => (response = res))
+    .catch((err) => console.log('fetchCoordinates Errored:', err));
+  console.log('fetchCoordinates response:', response);
+  return response;
 };
+console.log('TESTING FUNCTION:', fetchCoordinates('Minneapolis'));
 
 const fetchAPIs = { cityToWeather, fetchCoordinates, fetchWeather };
 export default fetchAPIs;
